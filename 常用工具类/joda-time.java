040108702e0af72512c0756bf1cc3293257eaa5a
bfchengnuo <bfchengnuo@gmail.com>
2018-12-06 17:14:17 +0800

    update

--- 常用工具类/joda-time.java
+++ 常用工具类/joda-time.java
@@ -33,3 +33,22 @@ private void fillingPlannedDate(PlanWeek planWeek, int weekCount) {
 	// 计划日期范围
 	planWeek.setPlanDateRange(range);
 }
+
+
+// 创建对象的方法
+DateTime dateTime1 = new DateTime();
+System.out.println(dateTime1); // out: 2016-02-26T16:02:57.582+08:00
+DateTime dateTime2 = new DateTime(2016,2,14,0,0,0);
+System.out.println(dateTime2); // out: 2016-02-14T00:00:00.000+08:00
+DateTime dateTime3 = new DateTime(1456473917004L);
+System.out.println(dateTime3); // out: 2016-02-26T16:05:17.004+08:00
+DateTime dateTime4 = new DateTime(new Date());
+System.out.println(dateTime4); // out: 2016-02-26T16:07:59.970+08:00
+DateTime dateTime5 = new DateTime("2016-02-15T00:00:00.000+08:00");
+System.out.println(dateTime5); // out: 2016-02-15T00:00:00.000+08:00
+
+DateFormat dateFormat = new SimpleDateFormat("yyyyMM");
+DateTime baseDate = new DateTime(dateFormat.parse("201812"));
+
+// plus/minus开头的方法进行加、减
+DateTime startDate = baseDate.minusMonths(2);
